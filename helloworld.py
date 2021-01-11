c2670998ec7b397fcb9251d9102ed41611d5d55c
TEJAS DIXIT <tejasdixit17@gmail.com>
2021-01-11 20:18:45 +0530

    created helloworld.py python file
    this is the first python lesson

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+#!/bin/python3.9 
+
+'''
+Note:-
+    There are major two versions of python 
+    that is python 2.x  and  python 3.x 
+    Now in this all following tutorial we will be using 
+    python 3.x 
+    
+    How to use ?
+    
+    If you want print the string or somethig 
+    you must have to use print funcation which will bultine 
+    in python 3.x version 
+    
+    '''
+# Syntax in python 3.x 
+print(" Hello world ") 
+
+# Syntax in python 2.x
+# print "Hello World " # Note we are not using this in for following all tutorials 
