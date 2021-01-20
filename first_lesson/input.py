5899be465d1ad7a9c1139416bb31947e609f7181
TEJAS DIXIT <tejasdixit17@gmail.com>
2021-01-20 11:11:31 +0530

    fixed indettion errors

--- first_lesson/input.py
+++ first_lesson/input.py
@@ -1,25 +1,25 @@
 note = '''
-  input()
-  syntax :-
-          object = input("<Message to display on screen>")
-          object  = funcation - message 
-          the taken input is being stored in object variable 
-          don't be upset we will dissuss more about variavles in 
-          upcomming leactures 
-  example :- 
-          my_name = input("Enter Your Name : )
-          print(my_name)
-  output  :- 
-          Enter Your Name : Tejas Dixit 
-          Tejas Dixit
-          
-          please don't include the <> symbols ,
-          in python you can use double or single quotes to print.
-          A) Today , we will learn about how to take input in python 
-          1. The input funcation will take  string parameter by deafult 
-          2. Yes, we can take integer input also , but we have to pass in
-             int() funcation. ''' 
-             
-  print(note)
-  my_name = input("Enter Your Name")
-  print(my_name)
+input()
+syntax :-
+        object = input("<Message to display on screen>")
+        object  = funcation - message 
+        the taken input is being stored in object variable 
+        don't be upset we will dissuss more about variavles in 
+        upcomming leactures 
+example :- 
+        my_name = input("Enter Your Name : )
+        print(my_name)
+output  :- 
+        Enter Your Name : Tejas Dixit 
+        Tejas Dixit
+
+        please don't include the <> symbols ,
+        in python you can use double or single quotes to print.
+        A) Today , we will learn about how to take input in python 
+        1. The input funcation will take  string parameter by deafult 
+        2. Yes, we can take integer input also , but we have to pass in
+           int() funcation. ''' 
+
+print(note)
+my_name = input("Enter Your Name")
+print(my_name)
