0dda161c5c5186bf50901ba5c335e5f1cfae2907
Christian Clauss <cclauss@me.com>
2021-04-27 23:49:37 +0200

    Python 2 died 16 months ago so let's not teach it anymore

--- first_lesson/helloworld.py
+++ first_lesson/helloworld.py
@@ -1,21 +1,14 @@
-#!/bin/python3.9 
+#!/usr/bin/env python3.9 
 
 '''
 Note:-
-    There are major two versions of python 
-    that is python 2.x  and  python 3.x 
-    Now in this all following tutorial we will be using 
-    python 3.x 
+    The following tutorial we will be using Python 3.9
     
     How to use ?
     
-    If you want print the string or somethig 
-    you must have to use print funcation which will bultine 
-    in python 3.x version 
+    If you want print the string or something  you have to
+    use the print() function which will builtin in Python 3.x.
     
     '''
-# Syntax in python 3.x 
+# Syntax in Python 3.x 
 print(" Hello world ") 
-
-# Syntax in python 2.x
-# print "Hello World " # Note we are not using this in for following all tutorials 
