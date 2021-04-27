af6c453bacb170be45505761ecab88bd81837ff4
Christian Clauss <cclauss@me.com>
2021-04-27 23:33:01 +0200

    python is a snake.  Python is a programming language.
    Explanation

--- first_lesson/input.py
+++ first_lesson/input.py
@@ -2,7 +2,7 @@ note = '''
 input()
 syntax :-
         object = input("<Message to display on screen>")
-        object  = funcation - message
+        object = function - message
         the taken input is being stored in object variable
         don't be upset we will dissuss more about variavles in
         upcomming leactures
@@ -13,14 +13,14 @@ output  :-
         Enter Your Name : Tejas Dixit
         Tejas Dixit
 
-        please don't include the <> symbols ,
-        in python you can use double or single quotes to print.
-        A) Today , we will learn about how to take input in python
-        1. The input funcation will take  string parameter by deafult
+        Please don't include the <> symbols ,
+        in Python you can use double or single quotes to print.
+        A) Today , we will learn about how to take input in Python
+        1. The input function will take  string parameter by default
         2. Yes, we can take integer input also , but we have to pass in
            int() funcation. '''
 
-input("Lets start lesson Press any key to start")
+input("Let's start lesson... Press any key to start")
 my_name = input("Enter Your Name : ")
 print(my_name)
 type_of_data = type(my_name)
@@ -36,12 +36,12 @@ code = '''my_age = int(input("Enter Your Age: "))
           print(my_age)
           print("myage is ",type(my_age),"type of data")'''
 
-my_age = int(input("Enter Your Age: "))
+my_age = int(input("Enter Your Age: "))  # type: ignore
 print(my_age)
 print("myage is ",type(my_age),"type of data")
 print("Now it shows int confused how is it happen.")
 input("For Explenetion press any key")
 print("code of following example")
 print(code)
-print("Explenetion: ")
+print("Explanation: ")
 print(note)
